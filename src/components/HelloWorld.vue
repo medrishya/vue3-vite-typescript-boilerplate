2c7a6fe53d1930e5c0eaaed87d39d5c613c79483
DrishyaChhetri <brewshell@BrewShell.local>
2024-05-06 11:00:00 +0545

    feat: project setup vue3, vite, ts, eslint, prettier, husky, lint staged

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0);
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      Edit out
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
